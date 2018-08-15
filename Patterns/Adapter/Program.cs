186b207e6a03ef75ff4aff55f96fa4a4b809337c
Surikenchik <popelyshkon25@gmail.com>
2018-08-16 01:48:08 +0300

    Adapter (object)

---
+++ Patterns/Adapter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Adapter
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Driver driver = new Adapter();
+            driver.GoToUrl("google.com");
+        }
+    }
+}
