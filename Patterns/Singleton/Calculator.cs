18fb14bea903d710828abf8b8263bb92f3d063e2
Surikenchik <popelyshkon25@gmail.com>
2018-08-15 21:19:21 +0300

    Singleton

---
+++ Patterns/Singleton/Calculator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Singleton
+{
+    public class Calculator
+    {
+        private static Calculator _calc;
+        private double res;
+
+        // Lock synchronization object
+        private static object syncLock = new object();
+
+        protected Calculator()
+        {
+        }
+
+        public static Calculator GetCalculator()
+        {
+            if (_calc == null)
+            {
+                lock (syncLock)
+                {
+                    if (_calc == null)
+                    {
+                        _calc = new Calculator();
+                    }
+                }
+            }
+            return _calc;
+        }
+
+        public double Operation (char op, double a, double b)
+        {
+            switch (op)
+            {
+                case '+': res = a + b;
+                    break;
+                case '-': res = a - b;
+                    break;
+                case '*': res = a * b;
+                    break;
+                case '/': res = a / b;
+                    break;
+                default:
+                    break;
+            }
+            return res;
+        }
+    }
+}
---
+++ Patterns/Singleton/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Singleton
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Calculator c1 = Calculator.GetCalculator();
+            Calculator c2 = Calculator.GetCalculator();
+
+            if (c1 == c2)
+            {
+                Console.WriteLine("Same instance");
+            }
+
+            Calculator calc = Calculator.GetCalculator();
+            double result = calc.Operation('+', 5, 8);
+            Console.WriteLine("Result is: " + result);
+            Console.ReadKey();
+        }
+    }
+}
